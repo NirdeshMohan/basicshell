3dd8c1441f4e8ab2572889ab9d082635a98541be
Nirdesh Mohan <NirdeshM@Gmail.com>
2020-04-07 22:25:18 -0400

    Update testscript.sh
    adding new comment

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
+echo "Will be adding $NUMBERONE and NUMBERTWO)
 echo "Addition of two numbers is :$NUMBER"
 echo "This was added"
