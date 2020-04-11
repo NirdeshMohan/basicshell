9d29832ba1e1f41e54d06c78d7f55faa926cd7ee
Nirdesh Mohan <NirdeshM@Gmail.com>
2020-04-11 00:36:53 -0400

    Update testscript.sh
    lastfix

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "this script is from github"
-NUMBER=$(($NUMBERONE + $NUMBERTWO))
+NUMBER=$($NUMBERONE + $NUMBERTWO)
 echo "Will be adding ($NUMBERONE and $NUMBERTWO)"
 echo "Addition of two numbers is :$NUMBER"
 echo "This was added 1....."
